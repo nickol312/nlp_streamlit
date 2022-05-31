ee73478d5693f4e94b2dbdeac742c9f912a6949f
nickol312 <72961151+nickol312@users.noreply.github.com>
2022-05-31 12:00:25 +0800

    Update nlp_app.py

--- nlp_app.py
+++ nlp_app.py
@@ -32,6 +32,7 @@ def predict(sentence ,tokenizer, max_length, padding_type, trunc_type):
     
 def main():
     st.title("NLP Sentiment Recognition")
+    st.subheader("Sentiment Labelled Sentences Data Sett")
     sente = st.text_area('Text to analyze',placeholder="Type Here")
     sentence = [sente]
     result=""
